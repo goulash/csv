84ba5243ca0f5bdd126874c843d94fc6ef9f07c3
Ben Morgan <neembi@gmail.com>
2016-01-03 18:34:19 +0100

    New: best-effort marshaling of slice of interface values
    Also refactored the code a little.

--- csv.go
+++ csv.go
@@ -28,7 +28,7 @@ var (
 func Marshal(v interface{}) ([]byte, error) {
 	vt := reflect.TypeOf(v)
 
-	// Sometimes Recorder and Marhaler are only defined on pointer types
+	// Check right away if it implements Marshaler or Recorder.
 	if vt.Implements(marshalerType) {
 		t := v.(Marshaler)
 		return t.MarshalCSV()
@@ -38,30 +38,30 @@ func Marshal(v interface{}) ([]byte, error) {
 		return marshalRecorder(t), nil
 	}
 
-	switch vt.Kind() {
-	case reflect.Ptr:
+	// Any of the other checks only make sense on non-pointers.
+	if vt.Kind() == reflect.Ptr {
 		return Marshal(reflect.ValueOf(v).Elem().Interface())
-	case reflect.Struct:
-		if vt.Implements(marshalerType) {
-			t := v.(Marshaler)
-			return t.MarshalCSV()
-		}
-		if vt.Implements(recorderType) {
-			t := v.(Recorder)
-			return marshalRecorder(t), nil
-		}
-		return nil, fmt.Errorf("csv: struct type %s does not implement Marshaler or Recorder", vt)
-	case reflect.Slice, reflect.Array:
-		if vt.Elem().Kind() == reflect.Ptr {
-			vt = vt.Elem() // now vt is a pointer
-		}
+	}
+
+	// We also support a slice or array of Recorder, but not of MarshalCSV, because
+	// semantically, anything could be in MarshalCSV, especially the header, and
+	// we don't want to try to guess.
+	if vt.Kind() == reflect.Slice || vt.Kind() == reflect.Array {
+		// Even if the slice has element type *Type, the following should still work.
 		if vt.Elem().Implements(recorderType) {
 			return marshalRecorderSlice(v)
 		}
+
+		// We might get a slice of some kind of interface, in which case we require that
+		// each element is of the same type.
+		if vt.Elem().Kind() == reflect.Interface {
+			return marshalInterfaceSlice(v)
+		}
+
 		return nil, fmt.Errorf("csv: slice element type %s does not implement Recorder", vt.Elem())
-	default:
-		return nil, fmt.Errorf("csv: cannot marshal type %s", vt)
 	}
+
+	return nil, fmt.Errorf("csv: cannot marshal %s", vt)
 }
 
 func marshalRecorder(v Recorder) []byte {
@@ -74,14 +74,56 @@ func marshalRecorder(v Recorder) []byte {
 func marshalRecorderSlice(v interface{}) ([]byte, error) {
 	vv := reflect.ValueOf(v)
 	n := vv.Len()
+	if n == 0 {
+		return nil, errors.New("csv: no data")
+	}
+
 	get := func(i int) Recorder {
 		return vv.Index(i).Interface().(Recorder)
 	}
 
 	var buf bytes.Buffer
+	writeRecord(&buf, get(0).Header())
+	for i := 0; i < n; i++ {
+		writeRecord(&buf, get(i).Record())
+	}
+	return buf.Bytes(), nil
+}
+
+// marshalInterfaceSlice takes a slice or array of an interface type.
+//
+// We require that each type in the slice/array is of the same type.
+func marshalInterfaceSlice(v interface{}) (bs []byte, err error) {
+	vv := reflect.ValueOf(v)
+	n := vv.Len()
 	if n == 0 {
 		return nil, errors.New("csv: no data")
 	}
+
+	t := reflect.TypeOf(vv.Index(0).Interface())
+	get := func(i int) Recorder {
+		r, ok := vv.Index(i).Interface().(Recorder)
+		if !ok {
+			panic(fmt.Errorf("csv: slice element %T does not implement Recorder", vv.Index(i).Interface()))
+		}
+		if rt := reflect.TypeOf(r); rt != t {
+			panic(fmt.Errorf("csv: expecting slice element type %s, got %s", t, rt))
+		}
+		return r
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			if e, ok := r.(error); ok {
+				err = e
+				return
+			}
+			// It's not an error, so continue panicking.
+			panic(r)
+		}
+	}()
+
+	var buf bytes.Buffer
 	writeRecord(&buf, get(0).Header())
 	for i := 0; i < n; i++ {
 		writeRecord(&buf, get(i).Record())
